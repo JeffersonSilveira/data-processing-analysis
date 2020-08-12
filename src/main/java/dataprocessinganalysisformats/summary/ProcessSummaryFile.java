90e202e1a8b610249ac5c7afaa2285993e08ba29
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-08-12 16:09:57 -0300

    Tratamento de objetos/writer

--- src/main/java/dataprocessinganalysisformats/summary/ProcessSummaryFile.java
+++ src/main/java/dataprocessinganalysisformats/summary/ProcessSummaryFile.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 
 import dataprocessinganalysisformats.comparator.ComparatorPiorVendedor;
+import dataprocessinganalysisformats.model.Customer;
 import dataprocessinganalysisformats.model.Sale;
 import dataprocessinganalysisformats.model.Salesman;
 import dataprocessinganalysisformats.model.SummaryImport;
@@ -20,43 +21,70 @@ public class ProcessSummaryFile {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProcessSummaryFile.class);
 	Gson gson = new Gson();
 
-
 	public SummaryImport summaryProcess(Object arquivo) {
+
 		LOGGER.info(" >> sumaryProcess");
+
 		
 		String returnLayout = this.validationLayout(arquivo);
-		
+
 		SummaryImport summary = new SummaryImport();
-		//summary.setPiorVendedor(this.getWorstSalesman(arquivo));
+		// summary.setPiorVendedor(this.getWorstSalesman(arquivo));
+		summary.setPiorVendedor(returnLayout);
 
 		LOGGER.info(" << sumaryProcess");
 
 		return summary;
 
 	}
-	
+
 	/**
 	 * Recupera o pior vendedor
 	 * 
 	 * @return String nome do vendedor
 	 */
-	private String validationLayout(Object arquivo) {
-		LOGGER.info(" >>  verifica layout");
+	public String validationLayout(Object arquivo) {
 
-		StringBuilder str = new StringBuilder();
+		try {
 
-		
-		Salesman salesman = gson.fromJson(arquivo.toString(), Salesman.class);
-		if(!salesman.getCpf().isEmpty()) {
-			
-			str.append(salesman);
+			LOGGER.info(" >>  verifica layout");
+
+			StringBuilder str = new StringBuilder();
+		    Integer	contadorSalesman = 0;
+		    Integer	contadorSale = 0;
+		    Integer	contadorCustomer = 0;
+
+			Salesman salesman = gson.fromJson(arquivo.toString(), Salesman.class);
+			Sale sale = gson.fromJson(arquivo.toString(), Sale.class);
+			Customer customer = gson.fromJson(arquivo.toString(), Customer.class);
+
+			if (salesman.getCpf() != null) {
+				str.append(salesman);
+				str.append("salesman:::"+ contadorSalesman++);
+			} else if (sale.getId() != null) {
+				str.append(sale);
+				str.append("sale:::"+ contadorSale++);
+
+				//{"id":"10","saleItems":[
+				 //{"id":1,"price":100,"quantity":10},
+				 //{"id":2,"price":2.50,"quantity":30},
+				 //{"id":3,"price":3.10,"quantity":40}],
+				//"salesmanName":"Pedro"}
+			} else if (customer.getCnpj() != null) {
+				str.append(customer);
+				str.append("customer:::"+ contadorCustomer++);
+
+			}
+
+			LOGGER.info(" << retorna layout");
+
+			return str.toString();
+		} catch (Exception e) {
+			e.getMessage();
 		}
-		
-		LOGGER.info(" << retorna layout");
-		return str.toString();
+		return null;
 	}
 
-	
 	/**
 	 * Recupera o pior vendedor
 	 * 
@@ -78,12 +106,12 @@ public class ProcessSummaryFile {
 
 	private Map<String, BigDecimal> summaryVenda(Object arquivo) {
 		LOGGER.info(" >> summaryVenda");
-		
+
 		Gson gson = new Gson();
 
 		Salesman obj = gson.fromJson(arquivo.toString(), Salesman.class);
 
 		LOGGER.info(" << summaryVenda");
-		return null;//vendasVendedor;
+		return null;// vendasVendedor;
 	}
 }
--- src/main/java/dataprocessinganalysisformats/writer/ReadingMultipleFormatFilesItemWriterConfig.java
+++ src/main/java/dataprocessinganalysisformats/writer/ReadingMultipleFormatFilesItemWriterConfig.java
@@ -1,18 +1,22 @@
 package dataprocessinganalysisformats.writer;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.LineAggregator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-import dataprocessinganalysisformats.model.SummaryImport;
-import dataprocessinganalysisformats.summary.ProcessSummaryFile;
+import com.google.gson.Gson;
+
+import dataprocessinganalysisformats.model.Salesman;
 
 @Configuration
 public class ReadingMultipleFormatFilesItemWriterConfig {
@@ -21,9 +25,10 @@ public class ReadingMultipleFormatFilesItemWriterConfig {
 
 	@Value("file:${input.files.location}${input.file.pattern.out}")
 	private Resource outputResource;
+	
+	Gson gson = new Gson();
 
-	
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
 	public ItemWriter leituraArquivoDelimitadoWriter() {
@@ -34,19 +39,22 @@ public class ReadingMultipleFormatFilesItemWriterConfig {
 
 	private LineAggregator lineAggregator() {
 
-		return new LineAggregator() {
-		
+		LineAggregator lineAggregator = new LineAggregator() {
 			
 			@Override
-			public String aggregate(Object item) {
-
-				LOGGER.info("Sumarizando informações...");
-				ProcessSummaryFile processSummaryFile = new ProcessSummaryFile(); 
-				SummaryImport summary = processSummaryFile.summaryProcess(item);
-
-				return summary.getPiorVendedor().toString();
+			public String aggregate(Object arquivo) {
+				
+				StringBuilder str = new StringBuilder();
+				str.append(arquivo);
+			
+				Salesman salesman = gson.fromJson(arquivo.toString(), Salesman.class);
+				
+				List<String> list = Arrays.asList(arquivo.toString());
+				List<String> answer = list.stream().map(String::toLowerCase).collect(Collectors.toList());
+				return "Texto aqui:::"+str.toString();
 			}
-
 		};
+
+		return lineAggregator;
 	}
 }
